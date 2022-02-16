6cc24397cae9012780ee553cab0ccf4da89e0016
webeli <pe_lias@msn.com>
2022-02-17 00:26:10 +0100

    Update Footer and Preview

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { FaGithub, FaTwitter, FaTelegramPlane } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className=" bg-white pt-5">
-      <div className="container mx-auto mt-8 px-8">
+    <footer className="bg-white pt-1">
+      <div className="container mx-auto mt-4 px-8">
         <div className="w-full flex flex-col md:flex-row py-6">
           <div className="flex-1 mb-6">
             <Image
@@ -15,7 +16,54 @@ export default function Footer() {
           </div>
 
           <div className="flex-1">
-            <p className="uppercase font-extrabold text-black md:mb-6">Links</p>
+            <p className="uppercase font-extrabold text-black md:mb-6">
+              Social
+            </p>
+            <ul className="list-reset mb-6">
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://t.me/rainbowkingdoms"
+                  className="flex font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FaTelegramPlane size="1.4em" className="mr-1" />
+                  Telegram
+                </a>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://twitter.com/RainbowKingdoms"
+                  className="flex font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FaTwitter size="1.4em" className="mr-1" />
+                  Twitter
+                </a>
+              </li>
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="https://github.com/Rainbow-Kingdoms"
+                  className="flex font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  <FaGithub size="1.4em" className="mr-1" />
+                  Github
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <p className="uppercase font-extrabold text-black md:mb-6">Blog</p>
+            <ul className="list-reset mb-6">
+              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                <a
+                  href="#"
+                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
+                >
+                  Medium
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <p className="uppercase font-extrabold text-black md:mb-6">More</p>
             <ul className="list-reset mb-6">
               <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                 <a
@@ -25,106 +73,10 @@ export default function Footer() {
                   FAQ
                 </a>
               </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Help
-                </a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Support
-                </a>
-              </li>
             </ul>
           </div>
           <div className="flex-1">
-            <p className="uppercase font-extrabold text-black md:mb-6">Legal</p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Terms
-                </a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Privacy
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase font-extrabold text-black md:mb-6">
-              Social
-            </p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Facebook
-                </a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Linkedin
-                </a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Twitter
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase font-extrabold text-black md:mb-6">
-              Company
-            </p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Official Blog
-                </a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  About Us
-                </a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
+            
           </div>
         </div>
       </div>
